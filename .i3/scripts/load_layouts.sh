9905a07568abbf9908ac091aca3fea8ee9a555ab
Gabriela Alexandra Moldovan <gabi_250@live.com>
2019-09-07 13:14:11 +0300

    Use workspace 1 instead.

--- .i3/scripts/load_layouts.sh
+++ .i3/scripts/load_layouts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-i3-msg "workspace 5; append_layout /home/gabi/.i3/workspaces/workspace-1.json"
+i3-msg "workspace 1; append_layout /home/gabi/.i3/workspaces/workspace-1.json"
 
 (xterm &)
 (gvim &)
