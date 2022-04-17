040a045aea8f6380af9363bc1e53ac7c63c401fd
Gabi Moldovan <gabi_250@live.com>
2022-04-17 13:57:43 +0100

    Hide untracked files when running dotfiles status.

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,1 +1,7 @@
-alias dotfiles "git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+function dotfiles
+    if test (count $argv) -gt 0 -a "$argv[1]" = "status"
+        git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME status --untracked-files=no $argv[2..]
+    else
+        git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
+    end
+end
