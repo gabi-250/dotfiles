4095dd91b4d97572ad6992dea1bbd387daad665a
Gabi Moldovan <gabi_250@live.com>
2022-05-02 02:05:40 +0100

    Preserve the yank after selection.

--- .config/nvim/lua/general/mappings.lua
+++ .config/nvim/lua/general/mappings.lua
@@ -1,3 +1,5 @@
 -- .vimrc quick open
 vim.api.nvim_set_keymap('n', '<Leader>ev', ':split $MYVIMRC<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>sov', ':source $MYVIMRC<CR>', { noremap = true })
+-- Preserve the yank post-selection
+vim.api.nvim_set_keymap('x', 'p', 'pgvy', { noremap = true })
