fdb6ade18a4710ee7857dd9cd5f0e292e7917958
Gabi Moldovan <gabi@gotpcrel.net>
2022-12-30 00:09:07 +0000

    Add dark theme for aerc, update toggle script.

--- .i3/scripts/toggle_alacritty_theme.sh
+++ .i3/scripts/toggle_alacritty_theme.sh
@@ -7,10 +7,16 @@ readonly ALACRITTY_COLORS=~/.config/alacritty/colors.yml
 readonly ALACRITTY_LIGHT=~/.config/alacritty/colors-light.yml
 readonly ALACRITTY_DARK=~/.config/alacritty/colors-dark.yml
 
+readonly AERC_COLORS=~/.config/aerc/stylesets/gruvbox
+readonly AERC_LIGHT=~/.config/aerc/stylesets/gruvbox-light
+readonly AERC_DARK=~/.config/aerc/stylesets/gruvbox-dark
+
 if [[ `readlink -f $ALACRITTY_COLORS` == `readlink -f $ALACRITTY_LIGHT` ]]; then
     ln -fs $ALACRITTY_DARK $ALACRITTY_COLORS
+    ln -fs $AERC_DARK $AERC_COLORS
 elif [[ `readlink -f $ALACRITTY_COLORS` == `readlink -f $ALACRITTY_DARK` ]]; then
     ln -fs $ALACRITTY_LIGHT $ALACRITTY_COLORS
+    ln -fs $AERC_LIGHT $AERC_COLORS
 fi
 
 touch $ALACRITTY_CONFIG
