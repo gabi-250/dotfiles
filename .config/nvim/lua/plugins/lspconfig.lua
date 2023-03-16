d2afef477b75913248af083651003d99302e1f54
Gabi Moldovan <gabi@gotpcrel.net>
2023-03-16 14:34:25 +0000

    Use a less annoying keybinding for code actions.

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -3,21 +3,27 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Enable rust_analyzer
 require'lspconfig'.rust_analyzer.setup({
-    capabilities=capabilities,
-    settings = {
-      -- to enable rust-analyzer settings visit:
-      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
-      ["rust-analyzer"] = {
-        -- enable clippy diagnostics on save
-        checkOnSave = {
-          command = "clippy"
-        },
+  capabilities=capabilities,
+  settings = {
+    -- to enable rust-analyzer settings visit:
+    -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+    ["rust-analyzer"] = {
+      -- enable clippy diagnostics on save
+      checkOnSave = {
+        command = "clippy"
+      },
+      cargo = {
+        features = "all",
+        extraEnv = {
+          CARGO_TARGET_DIR = "./target/ra"
+        }
       }
     }
+  }
 })
 
 require'lspconfig'.clangd.setup({
-    capabilities=capabilities,
+  capabilities=capabilities,
 })
 
 -- Enable diagnostics
@@ -33,7 +39,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 vim.api.nvim_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {})
 vim.api.nvim_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', {})
 vim.api.nvim_set_keymap('n', 'gs', '<cmd>vsplit<CR><cmd>lua vim.lsp.buf.definition()<CR>', {})
-vim.api.nvim_set_keymap('n', 'ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
+vim.api.nvim_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {})
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', {})
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', {})
 vim.api.nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', {})
