628f461806b05b0370ff7b59cbd94fe7f847e6ec
Gabi Moldovan <gabi@gotpcrel.net>
2023-11-05 00:10:48 +0000

    Update dotfiles function.

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,6 +1,10 @@
 function dotfiles
     if test (count $argv) -gt 0 -a "$argv[1]" = "status"
-        git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME status --untracked-files=no $argv[2..]
+        if test "$argv[2]"
+            git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME status --untracked-files=no $argv[2..]
+        else
+            git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME status --untracked-files=no
+        end
     else
         git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
     end
